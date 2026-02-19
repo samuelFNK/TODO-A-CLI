492bcd165e903fce04cf31bf0e0c93beca5b5f7e
samuel.funck <samuel.funck@outlook.com>
2026-02-19 15:18:04 +0100

    Added functional roller menu with view, add, delete, remove. And a JSON 'database' to keep track of the todo

--- bin/index.js
+++ bin/index.js
@@ -1,3 +1,63 @@
 #!/usr/bin/env node
 
-console.log("TODO or not TODO, twas not the issue.");
+import {intro, outro, select, text} from '@clack/prompts';
+import { loadTodo, saveTodo } from '../lib/db.js';
+
+
+async function main() {
+    intro('TODO-A-CLI');
+
+    const action = await select({
+        message: 'What todo?',
+        options: [
+            {value: 'view', label: 'View todo list.'},
+            {value: 'add', label: 'Add something todo.'},
+            {value: 'remove', label: 'Remove a todo.'},
+            {value: 'exit', label: 'Leave.'}
+        ]
+    });
+
+    const todo = loadTodo();
+
+    if (action === 'view') {
+        console.log('TODO:');
+        todo.forEach((item, i) => console.log(`${i + 1}. ${item}`));
+    }
+
+
+    if (action === 'add') {
+        const newItem = await text({
+            message: 'What would you like todo?'
+        });
+
+        todo.push(newItem);
+        saveTodo(todo);
+
+        console.log('Added.')
+    }
+
+
+    if (action === 'remove') {
+        if (todo.length === 0) {
+            console.log('No todos to remove.');
+        } else {
+            const index = await select({
+                message: 'Which todo to remove?',
+                options: todo.map((item, i) => ({
+                    value: i,
+                    label: item
+                }))
+            });
+
+            todo.splice(index, 1);
+            saveTodo(todo);
+
+            console.log('Todo removed.');
+        }
+    }
+
+    
+    outro('A-CLI-WAS-DONE');
+}
+
+main();
